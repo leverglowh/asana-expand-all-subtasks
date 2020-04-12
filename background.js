753a0b7aa26fd301d3dcf0d7e1ed74a62a846eea
yingying.wu <yingying.wu@techrain.eu>
2020-04-12 04:46:13 +0200

    Fix bug

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript(tab.id,{
         code:
-          "const elements = document.getElementsByClassName('SpreadsheetTaskNameCell-subtaskToggleButton'); console.log(elements.length);Array.prototype.forEach.call(elements, e => e.click()); let more = document.getElementsByClassName('SpreadsheetTaskList-showMoreLink'); while(more.length>0) {more[0].click(); more = document.getElementsByClassName('SpreadsheetTaskList-showMoreLink');}"
+          "const elements = document.getElementsByClassName('SpreadsheetTaskNameCell-subtaskToggleButton');let len = elements.length;for (let i = 0; i < len; i++) {elements[i].click();} let timer = setInterval(()=>{const more = document.getElementsByClassName('SpreadsheetTaskList-showMoreLink'); if (more.length==0) {clearInterval(timer);} more[0].click();}, 10);"
     });
 });
